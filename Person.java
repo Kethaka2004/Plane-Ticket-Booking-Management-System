7bfe000a26fae772e99756db7060db1837d150aa
Kethaka Adithya Weerasinghe <kethakaadithya979@gmail.com>
2025-11-23 21:47:03 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,43 @@
+public class Person {
+
+    //store name, surname, email
+    private String name;
+    private String surname;
+    private String email;
+
+    //Constructs a new Person object with the specified name, surname, and email
+    public Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //method to print information about person
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Email: " + email);
+    }
+}
